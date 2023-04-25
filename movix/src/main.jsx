345e8519510e6a32a925518b78a32aba19c17a88
Anish Gupta <anish.gupta05497@gmail.com>
2023-04-25 16:57:52 +0530

    changes the main.jsx to link the redux store

--- movix/src/main.jsx
+++ movix/src/main.jsx
@@ -1,10 +1,14 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
 
+import { store } from './store/store.js'
+import { Provider } from 'react-redux'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>,
+);
