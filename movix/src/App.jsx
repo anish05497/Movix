3d59a408130a9e4b325f0f2b7cba77bba45ad22a
Anish Gupta <anish.gupta05497@gmail.com>
2023-04-25 16:58:34 +0530

    Added all the components in App.jsx

--- movix/src/App.jsx
+++ movix/src/App.jsx
@@ -1,9 +1,24 @@
 import { useEffect } from 'react'
-
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css'
 import { fetchDataFromApi } from './utils/api';
+import { useSelector, useDispatch } from 'react-redux'
+import { getApiConfiguration } from './store/homeSlice';
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from './pages/home/Home';
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from './pages/404/pageNotFound';
+
 
 function App() {
+
+  const dispatch = useDispatch()
+  const { url } = useSelector((state)=>state.home);
+  console.log(url)
   
   useEffect(()=>{
     apiTesting();
@@ -12,13 +27,22 @@ function App() {
   const apiTesting = () => {
     fetchDataFromApi("/movie/popular").then((res)=>{
       console.log(res);
+      dispatch(getApiConfiguration(res))
     })
   }
 
   return (
-    <div className="App">
-      App
-    </div>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/:mediaType/:id" element={<Details />} />
+      <Route path="/search/:query" element={<SearchResult />} />
+      <Route path="/explore/:mediaType" element={<Explore />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+    <Footer/>
+    </BrowserRouter>
   )
 }
 
